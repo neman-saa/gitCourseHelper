e309c28cb921694111d1495c98d7e2bbc099712a
neman-saa <neman.saa@gmail.com>
2023-04-07 17:22:09 +0300

    added 2 objects

--- src/main/scala/Object1.scala
+++ src/main/scala/Object1.scala
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 object Object1 {
 
+=======
+object Object1 extends App {
+case class Person(name: String, age: Int)
+  val jack = Person("Jacky", 25)
+>>>>>>> 6584e96 (added 2 objects)
 }
